fb89984f3ab890884c6eb310904eb3aeab83fbe4
e.blagodarev <ze0nni@gmail.com>
2020-04-19 11:01:21 +0300

    Prepare GameScene

---
+++ Assets/Code/Setup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour
+{
+    public GameObject Plane;
+
+    public GameObject bombPrefab;
+    public GameObject characterPrefab;
+}
