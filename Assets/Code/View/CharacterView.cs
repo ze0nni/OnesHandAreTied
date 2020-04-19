d5b3d4afc1bc230185f85178d2b321e8e334ab8f
e.blagodarev <ze0nni@gmail.com>
2020-04-19 15:35:34 +0300

    Add healthbar to 'CharacterView'

--- Assets/Code/View/CharacterView.cs
+++ Assets/Code/View/CharacterView.cs
@@ -6,4 +6,20 @@ using UnityEngine;
 public class CharacterView : MonoBehaviour
 {
     public EcsEntity entity;
+
+    public float health;
+
+    private SpriteRenderer bar;
+    void Start() {
+        this.bar = transform.Find("Health").Find("Bar").GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        this.bar.transform.localScale = new Vector3(
+            Mathf.Max(0, this.health),
+            1,
+            1
+        );
+    }
 }
